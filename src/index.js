00f5850d597889c5176416016898f26bb62d914e
Administrator <CZW0919@163.com>
2020-05-02 16:01:07 +0800

    初始化

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'lib-flexible';
 import * as serviceWorker from './serviceWorker';
+import Router from './routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
