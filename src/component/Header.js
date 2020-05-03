2f14e682f0542a7e8eeb306042dbe45047afd565
Administrator <yangfanqikang@163.com>
2020-05-03 21:11:30 +0800

    修改

--- src/component/Header.js
+++ src/component/Header.js
@@ -12,44 +12,47 @@ export default class Header extends Component {
         }
     }
 
+    render() {
+        const {isShow} = this.state;
+        return (
+            <div>
+                <div className='header-blank'></div>
+                <header>
+                    <div className='logo'><img src={logo} alt=""/></div>
+                    <div className='nav'>
+                        <div className='nav-icon' onClick={() => this.triggerMenu()}>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        {isShow ? <div className='nav-menu'>
+                            <NavLink className='item' exact activeClassName='active' activeStyle={{
+                                color: 'rgb(0,103,168)',
+                                backgroundColor: 'white'
+                            }} to='/'>首页</NavLink>
+                            <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
+                                backgroundColor: 'white'}} to='/aboutUs'>关于我们</NavLink>
+                            <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
+                                backgroundColor: 'white'}} to='/business'>业务模块</NavLink>
+                            <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
+                                backgroundColor: 'white'}} to='/partner'>成功案例</NavLink>
+                            <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
+                                backgroundColor: 'white'}} to='/team'>团队文化</NavLink>
+                            <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
+                                backgroundColor: 'white'}} to='/contact'>联系我们</NavLink>
+                        </div> : null }
+                    </div>
+                </header>
+            </div>
+    )
+    }
+
+
     triggerMenu() {
         console.log(window.location)
         this.setState({
             isShow: !this.state.isShow
         })
-    }
-
-    
-    render() {
-        const {isShow} = this.state;
-        return (
-            <header>
-                <div className='logo'><img src={logo} alt=""/></div>
-                <div className='nav'>
-                    <div className='nav-icon' onClick={() => this.triggerMenu()}>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    {isShow ? <div className='nav-menu'>
-                        <NavLink className='item' exact activeClassName='active' activeStyle={{
-                            color: 'rgb(0,103,168)',
-                            backgroundColor: 'white'
-                        }} to='/'>首页</NavLink>
-                        <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
-                            backgroundColor: 'white'}} to='/aboutUs'>关于我们</NavLink>
-                        <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
-                            backgroundColor: 'white'}} to='/business'>业务模块</NavLink>
-                        <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
-                            backgroundColor: 'white'}} to='/partner'>成功案例</NavLink>
-                        <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
-                            backgroundColor: 'white'}} to='/team'>团队文化</NavLink>
-                        <NavLink className='item' activeClassName='active' activeStyle={{color: 'rgb(0,103,168)',
-                            backgroundColor: 'white'}} to='/contact'>联系我们</NavLink>
-                    </div> : null }
-                </div>
-            </header>
-        )
     }
 }
 
